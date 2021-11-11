28f1cd22b8dc04e0498eb279fd75f2b4a8b0ab99
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-11-11 14:10:15 +0100

    basic alphabet and tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,28 @@
 mod typedefs;
+mod alphabet;
 
 #[cfg(test)]
 mod tests {
+    use crate::alphabet::SimpleAlphabet;
+
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn create_alphabet_from_vector() {
+        SimpleAlphabet::new(vec!['A', 'C', 'T', 'G', '-']);
+    }
+
+    #[test]
+    fn index_alphabet_with_number() {
+        let alph = SimpleAlphabet::new(vec!['A', 'C', 'T', 'G', '-']);
+
+        assert_eq!(alph[0], 'A');
+        assert_eq!(alph[4], '-');
+    }
+
+    #[test]
+    fn index_alphabet_with_letter() {
+        let alph = SimpleAlphabet::new(vec!['A', 'C', 'T', 'G', '-']);
+
+        assert_eq!(alph['A'], 0);
+        assert_eq!(alph['C'], 1);
     }
 }
