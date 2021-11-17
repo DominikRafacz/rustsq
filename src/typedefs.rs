3c2828f03af34a92dca17b72f43efa9863ac2c60
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-11-17 12:30:37 +0100

    types for internal data

--- src/typedefs.rs
+++ src/typedefs.rs
@@ -3,6 +3,9 @@ pub type Asize = u8;
 pub type Letter = &'static str;
 pub type SimpleLetter = char;
 
+pub(crate) type ProtoSequenceData = String;
+pub(crate) type SequenceData = Vec<ElemPacked>;
+
 pub type ElemPacked = u8;
 
 pub enum SqType {
