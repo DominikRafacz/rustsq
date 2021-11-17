ef89162fcca9b62297a04a72a2df699b5cc89cbe
DominikRafacz <d.rafacz@student.mini.pw.edu.pl>
2021-11-17 12:30:18 +0100

    calc packed len

--- src/utilities/calc.rs
+++ src/utilities/calc.rs
@@ -1,5 +1,10 @@
+use crate::alphabet::Alphabet;
 use crate::typedefs::Asize;
 
 pub(crate) fn asize(alph_len: usize) -> Asize {
     ((alph_len + 2) as f32).log2().ceil() as Asize
+}
+
+pub(crate) fn packed_len(proto_data_len: usize, alph: &Alphabet) -> usize {
+    (proto_data_len as f32 / alph.asize() as f32).ceil() as usize
 }
